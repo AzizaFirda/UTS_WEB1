a21b40f8f926f09a107398f40154d22ea4b023fe
Kk Ade <azizafirdaus07@gmail.com>
2025-11-21 15:28:48 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+function loginValidation() {
+let user = document.getElementById("username").value;
+let pass = document.getElementById("password").value;
+
+
+if (user === "admin" && pass === "12345") {
+window.location.href = "dashboard.html";
+return false;
+} else {
+alert("Username atau password salah!");
+return false;
+}
+}
